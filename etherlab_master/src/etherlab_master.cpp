bd84991d8d502b7fbfd713287dda88f45644113d
MAX <zhiyong@smart.mit.edu>
2014-02-04 09:33:14 +0800

    add sdo

--- etherlab_master/src/etherlab_master.cpp
+++ etherlab_master/src/etherlab_master.cpp
@@ -14,15 +14,158 @@
 /****************************************************************************/
 #define AliasAndPositon  0, 0
 #define VendorID_ProductCode  0x000000e4, 0x00001133
+
+// Application parameters
+#define FREQUENCY 100
+#define PRIORITY 1
+// Optional features
+#define CONFIGURE_PDOS  1
+#define SDO_ACCESS      1
+
+#if SDO_ACCESS
+static ec_sdo_request_t *sdo;
+#endif
+
 // EtherCAT
 static ec_master_t *master = NULL;
+static ec_master_state_t master_state = {};
 
 static ec_domain_t *domain1 = NULL;
+static ec_domain_state_t domain1_state = {};
 
 static ec_slave_config_t *sc_ana_in = NULL;
+static ec_slave_config_state_t sc_ana_in_state = {};
+
+static unsigned int counter = 0;
+static unsigned int blink = 0;
+/*****************************************************************************/
+
+#if SDO_ACCESS
+void read_sdo(void)
+{
+    switch (ecrt_sdo_request_state(sdo)) {
+        case EC_REQUEST_UNUSED: // request was not used yet
+            ecrt_sdo_request_read(sdo); // trigger first read
+            break;
+        case EC_REQUEST_BUSY:
+            fprintf(stderr, "Still busy...\n");
+            break;
+        case EC_REQUEST_SUCCESS:
+            fprintf(stderr, "SDO value: 0x%04X\n",
+                    EC_READ_U16(ecrt_sdo_request_data(sdo)));
+            ecrt_sdo_request_read(sdo); // trigger next read
+            break;
+        case EC_REQUEST_ERROR:
+            fprintf(stderr, "Failed to read SDO!\n");
+            ecrt_sdo_request_read(sdo); // retry reading
+            break;
+    }
+}
+#endif
+
+/*****************************************************************************/
+
+void check_domain1_state(void)
+{
+    ec_domain_state_t ds;
+
+    ecrt_domain_state(domain1, &ds);
+
+    if (ds.working_counter != domain1_state.working_counter)
+        printf("Domain1: WC %u.\n", ds.working_counter);
+    if (ds.wc_state != domain1_state.wc_state)
+        printf("Domain1: State %u.\n", ds.wc_state);
+
+    domain1_state = ds;
+}
+
+/*****************************************************************************/
+
+void check_master_state(void)
+{
+    ec_master_state_t ms;
+
+    ecrt_master_state(master, &ms);
+
+    if (ms.slaves_responding != master_state.slaves_responding)
+        printf("%u slave(s).\n", ms.slaves_responding);
+    if (ms.al_states != master_state.al_states)
+        printf("AL states: 0x%02X.\n", ms.al_states);
+    if (ms.link_up != master_state.link_up)
+        printf("Link is %s.\n", ms.link_up ? "up" : "down");
+
+    master_state = ms;
+}
+/*****************************************************************************/
+
+void check_slave_config_states(void)
+{
+    ec_slave_config_state_t s;
+
+    ecrt_slave_config_state(sc_ana_in, &s);
+
+    if (s.al_state != sc_ana_in_state.al_state)
+        printf("AnaIn: State 0x%02X.\n", s.al_state);
+    if (s.online != sc_ana_in_state.online)
+        printf("AnaIn: %s.\n", s.online ? "online" : "offline");
+    if (s.operational != sc_ana_in_state.operational)
+        printf("AnaIn: %soperational.\n",
+                s.operational ? "" : "Not ");
+
+    sc_ana_in_state = s;
+}
 
 /****************************************************************************/
 
+void cyclic_task()
+{
+    // receive process data
+    ecrt_master_receive(master);
+    ecrt_domain_process(domain1);
+
+    // check process data state (optional)
+    check_domain1_state();
+
+    if (counter) {
+        counter--;
+    } else { // do this at 1 Hz
+        counter = FREQUENCY;
+
+        // calculate new process data
+        blink = !blink;
+
+        // check for master state (optional)
+        check_master_state();
+
+        // check for islave configuration state(s) (optional)
+        check_slave_config_states();
+
+#if SDO_ACCESS
+        // read process data SDO
+        read_sdo();
+#endif
+
+    }
+
+#if 0
+    // read process data
+    printf("AnaIn: state %u value %u\n",
+            EC_READ_U8(domain1_pd + off_ana_in_status),
+            EC_READ_U16(domain1_pd + off_ana_in_value));
+#endif
+
+#if 1
+    // write process data
+//    EC_WRITE_U8(domain1_pd + off_dig_out, blink ? 0x06 : 0x09);
+#endif
+
+    // send process data
+    ecrt_domain_queue(domain1);
+    ecrt_master_send(master);
+}
+
+
+/*****************************************************************************/
 int main(int argc, char **argv)
 {
     ec_slave_config_t *sc;
@@ -44,5 +187,15 @@ int main(int argc, char **argv)
         fprintf(stderr, "Failed to get slave configuration.\n");
         return -1;
     }
+
+#if SDO_ACCESS
+    fprintf(stderr, "Creating SDO requests...\n");
+    if (!(sdo = ecrt_slave_config_create_sdo_request(sc_ana_in, 0x6061, 0, 1))) // data size 1 ?
+    {
+        fprintf(stderr, "Failed to create SDO modes_of_operation_display 0x6061 request.\n");
+        return -1;
+    }
+    ecrt_sdo_request_timeout(sdo, 500); // ms
+#endif
 
 }
